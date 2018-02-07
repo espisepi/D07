10b381fd0135d400b6ea4fdfdcc8f7ebe137d72c
espisepi <sepinaco@gmail.com>
2018-02-07 19:49:15 +0100

    Añadida la segunda Query en el NotificationRepository

--- NIVEL_C/Acme-Explorer/src/main/java/repositories/NotificationRepository.java
+++ NIVEL_C/Acme-Explorer/src/main/java/repositories/NotificationRepository.java
@@ -17,7 +17,7 @@ public interface NotificationRepository extends JpaRepository<Notification, Inte
 	@Query("select (select count(t) from Trip t where t.notifications.size!=0) * 1.0 / count(t) from Trip t")
 	Double findRatioTripsWithNotifications();
 
-	@Query("select m from Manager m")
+	@Query("select t from Trip t where t.notifications.size=(select max(t.notifications.size) from Trip t)")
 	Collection<Manager> findManagersWithMoreNotifications();
 
 	@Query("select t from Trip t join t.notifications n where n.id=?1")
