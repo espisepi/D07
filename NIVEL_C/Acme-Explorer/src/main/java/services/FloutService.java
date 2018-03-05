9d23a42167d584539be4aaa1eb6420a765a4da72
espisepi <sepinaco@gmail.com>
2018-03-05 20:13:14 +0100

    feat: añadido todos los caracteres en el ticker

--- NIVEL_C/Acme-Explorer/src/main/java/services/FloutService.java
+++ NIVEL_C/Acme-Explorer/src/main/java/services/FloutService.java
@@ -68,11 +68,12 @@ public class FloutService {
 
 		//Añadimos las dos primeras letras
 		final char[] arr = new char[] {
-			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+			'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z'
 		};
 		String cadenaAleatoria = "";
 		for (Integer i = 0; i <= 1; i++) {
-			final char elegido = arr[(int) (Math.random() * 26)];
+			final char elegido = arr[(int) (Math.random() * 61)];
 			cadenaAleatoria = cadenaAleatoria + elegido;
 
 		}
